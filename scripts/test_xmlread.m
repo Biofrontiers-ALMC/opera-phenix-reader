37845fb7a5022abdf5c0295ef83d5a240f1ae680
Jian Tay <jian.tay@colorado.edu>
2024-07-08 16:33:53 -0600

    Updated code to read metadata

--- scripts/test_xmlread.m
+++ scripts/test_xmlread.m
@@ -1,11 +1,20 @@
 DOMnode = xmlread('F:\2024 Liu Lab\Duration data + HeCAT21624__2024-02-16T15_55_35-Measurement 1\Images\Index.idx.xml');
-
-% doc = DOMnode.getChildNodes;
-
+% 
+% % doc = DOMnode.getChildNodes;
+% 
 import javax.xml.xpath.*
 factory = XPathFactory.newInstance;
 xpath = factory.newXPath;
 
-expression = xpath.compile('//EvaluationInputData/Plates/Plate/PlateID');
+expression = xpath.compile('//Plates');
 plateIDnode = expression.evaluate(DOMnode, XPathConstants.NODE);
 output = plateIDnode.getTextContent
+
+% T = readstruct('F:\2024 Liu Lab\Duration data + HeCAT21624__2024-02-16T15_55_35-Measurement 1\Images\Index.idx.xml',...
+%     StructSelector="//Plates");
+
+% import matlab.io.xml.xpath.*
+% 
+% xpExpr = "//PlateRows";
+% xmlFilePath = "F:\2024 Liu Lab\Duration data + HeCAT21624__2024-02-16T15_55_35-Measurement 1\Images\Index.idx.xml";
+% data = evaluate(Evaluator,xpExpr,xmlFilePath,EvalResultType.NodeSet)
--- tbx/OPreader/OPreader.m
+++ tbx/OPreader/OPreader.m
@@ -3,7 +3,17 @@ classdef OPreader
     properties (SetAccess = private)
 
         folder
-        
+        index
+
+        PlateName
+        MeasurementStartTime
+        MeasurementStartTimeLocal
+
+        NumRows
+        NumCols
+        PlateMap
+
+        WellInfo
 
     end
 
@@ -20,20 +30,88 @@ classdef OPreader
                 if ~exist(inputDir, 'dir')
                     error('OPreader:DirectoryDoesNotExist', ...
                         'Directory %s does not exist.', inputDir)
-                end
 
-                files
+                else
+                    %Look for the index file
+                    if exist(fullfile(inputDir, 'Images', 'Index.idx.xml'), 'file')
 
+                        obj.folder = inputDir;
 
+                    elseif exist(fullfile(inputDir, 'Index.idx.xml'), 'file')
+
+                        fpath = fileparts(inputDir);
+
+                        obj.folder = fpath;
+
+                    else
+
+                        error('OPreader:CannotFindIndex', ...
+                        'Could not find Index.idx.xml file in folder %s.', inputDir);
+                    end
+
+                    obj = parseMetadataXML(obj);
+
+                end                
 
             end
+        end
+    end
+
+    methods (Access = protected)
+
+        function obj = parseMetadataXML(obj)
+
+            %Read in metadata
+            fprintf('Reading metadata file... please wait.\n')
+            S = readstruct(fullfile(obj.folder, 'Images', 'Index.idx.xml'));
+
+            %Parse the metadata
+            obj.PlateName = S.Plates.Plate.Name;
+            obj.MeasurementStartTime = S.Plates.Plate.MeasurementStartTime;
+
+            dt = datetime('now', 'TimeZone', 'local');
+            tz = dt.TimeZone;
+            obj.MeasurementStartTimeLocal = datetime(obj.MeasurementStartTime,'InputFormat','yyyy-MM-dd''T''HH:mm:ss.SSSSSSSZ','TimeZone',tz);
+
+            obj.NumRows = S.Plates.Plate.PlateRows;
+            obj.NumCols = S.Plates.Plate.PlateCols;
+
+            %Gather image information
+            imgRows = cat(1, S.Images.Image.Col);
+            imgCols = cat(1, S.Images.Image.Col);
+
+            obj.PlateMap = false(obj.NumRows, obj.NumCols);
+            obj.WellInfo = struct('Fields', [], 'Planes', [], ...
+                'Timepoints', [], 'Channels', [], 'ChannelNames', [], ...
+                'Images', []);
+            
+            %See which wells are populated
+            for iWell = 1:numel(S.Plates.Plate.Well)
+
+                %The well ID is converted into a number. First digit (or
+                %two digits for 4 digit number) is Row, last two digits are
+                %column.
+
+                wellIDstr = sprintf('%04d', S.Plates.Plate.Well(iWell).idAttribute);
+
+                row = str2double(wellIDstr(1:2));
+                col = str2double(wellIDstr(3:4));
+
+                obj.PlateMap(row, col) = true;
+
+                %Pull image information
+                tmpS = S.Images.Image(imgRows == row && imgCols == col);
+
+                obj.WellInfo(row, col).Fields = unique(cat(1, tmpS.FieldID));
+                obj.WellInfo(row, col).Planes = unique(cat(1, tmpS.PlaneID));
+                obj.WellInfo(row, col).Timepoints = unique(cat(1, tmpS.TimepointID));
+                
 
 
 
-
+            end    
 
         end
-    
 
     end
 
