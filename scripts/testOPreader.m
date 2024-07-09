27bc713a8f1110bd5ec055554dddb1db4f610da4
Jian Tay <jian.tay@colorado.edu>
2024-07-09 15:26:07 -0600

    Updated image reading

--- scripts/testOPreader.m
+++ scripts/testOPreader.m
@@ -1,6 +1,21 @@
 clearvars
 clc
 
-OPR = OPreader('D:\Documents\OneDrive - UCB-O365\Storage\OPreader test');
+%OPR = OPreader('D:\Documents\OneDrive - UCB-O365\Storage\OPreader test');
 
+OPR = OPreader('F:\2024 Liu Lab\Duration data + HeCAT21624__2024-02-16T15_55_35-Measurement 1');
 
+%%
+
+Inucl = readImage(OPR, 7, 2, 1, 1);
+Icell = readImage(OPR, 7, 2, 2, 1);
+
+Inucl = double(Inucl);
+Inucl = (Inucl - min(Inucl(:)))/(max(Inucl(:)) - min(Inucl(:)));
+
+Icell = double(Icell);
+Icell = (Icell - min(Icell(:)))/(max(Icell(:)) - min(Icell(:)));
+
+Irgb = cat(3, Inucl, Icell, zeros(size(Inucl), 'double'));
+
+imshow(Irgb)
--- tbx/OPreader/OPreader.m
+++ tbx/OPreader/OPreader.m
@@ -58,11 +58,12 @@ classdef OPreader
             imageIdx = [obj.ImageInfo.Row] == row & ...
                 [obj.ImageInfo.Col] == col & ...
                 [obj.ImageInfo.ChannelID] == channel & ...
-                [obj.ImageInfo.TimepointID] == timepoint & ...
+                [obj.ImageInfo.TimepointID] == (timepoint - 1) & ...
                 [obj.ImageInfo.FieldID] == 1 & ...
                 [obj.ImageInfo.PlaneID] == 1;
 
             if nnz(imageIdx) == 0
+
                 error('OPreader:readImage:InvalidImageCoordinate', ...
                     'Image at the specified coordinate does not exist.')
 
@@ -73,10 +74,81 @@ classdef OPreader
 
             end
 
-            I = obj.ImageInfo(imageIdx).URL;
+            I = imread(fullfile(obj.folder, 'Images', obj.ImageInfo(imageIdx).URL));
             
         end
 
+        function imshow(obj, row, col, timepoint)
+
+            Inucl = readImage(obj, row, col, 1, timepoint);
+            Icell = readImage(obj, row, col, 2, timepoint);
+
+            Inucl = double(Inucl);
+            Inucl = (Inucl - min(Inucl(:)))/(max(Inucl(:)) - min(Inucl(:)));
+
+            Icell = double(Icell);
+            Icell = (Icell - min(Icell(:)))/(max(Icell(:)) - min(Icell(:)));
+
+            Irgb = cat(3, Inucl, Icell, zeros(size(Inucl), 'double'));
+
+            imshow(Irgb)
+        end
+
+        function exportVideo(obj, row, col, channel, varargin)
+            %EXPORTVIDEO  Export video
+            %
+            %  EXPORTVIDEO(OBJ, ROW, COL, CHANNEL)
+
+            if ~isempty(varargin)
+                outputFN = varargin{1};
+            else
+                outputFN = 'export.avi';
+            end
+
+            %Check if output folder exists
+            fpath = fileparts(outputFN);
+
+            if ~exist(fpath, 'dir') && ~isempty(fpath)
+                mkdir(fpath);
+            end
+
+            %Get number of timepoints
+            s = unique(cat(1, obj.ImageInfo([obj.ImageInfo.Row] == row & [obj.ImageInfo.Col] == col).TimepointID));
+
+            vid = VideoWriter(outputFN);
+            open(vid)
+
+            for iT = 1:numel(s)
+
+                I = readImage(obj, row, col, channel, iT);
+
+                I = double(I);
+                I = (I - min(I(:)))/(max(I(:)) - min(I(:)));
+                
+                writeVideo(vid, I);
+
+            end
+            close(vid)
+
+        end
+
+        function wellInfo = getWellInfo(obj, row, col)
+
+            if ~obj.PlateMap(row, col)
+                error('OPreader:getWellInfo:NoImagesInWell', ...
+                    'Specified well does not contain images.')
+            end
+
+            %Get well data
+            s = obj.ImageInfo([obj.ImageInfo.Row] == row & [obj.ImageInfo.Col] == col);
+
+            wellInfo.NumImages = numel(s);
+            wellInfo.NumFields = numel(unique([s.FieldID]));
+            wellInfo.NumPlanes = numel(unique([s.PlaneID]));
+            wellInfo.NumTimepoints = numel(unique([s.TimepointID]));
+            wellInfo.NumChannels = numel(unique([s.ChannelID]));
+
+        end
     end
 
     methods (Access = protected)
@@ -106,7 +178,10 @@ classdef OPreader
             %Populate some overview data
             wellLocs = unique(imgLoc, 'rows');
 
-            obj.PlateMap(wellLocs(:, 1), wellLocs(:, 2)) = true;
+            for iLoc = 1:size(wellLocs, 1)
+                obj.PlateMap(wellLocs(iLoc, 1), wellLocs(iLoc, 2)) = true;
+            end
+            
 
             % 
             % %See which wells are populated
