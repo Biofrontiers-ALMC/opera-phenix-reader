3e8244a61617942ec8abdb7844854ec957c438e5
Jian Wei Tay <jian.tay@colorado.edu>
2024-07-09 10:44:17 -0600

    Updated metadata import

---
+++ scripts/testOPreader.m
@@ -0,0 +1,6 @@
+clearvars
+clc
+
+OPR = OPreader('D:\Documents\OneDrive - UCB-O365\Storage\OPreader test');
+
+
--- tbx/OPreader/OPreader.m
+++ tbx/OPreader/OPreader.m
@@ -8,15 +8,12 @@ classdef OPreader
         PlateName
         MeasurementStartTime
         MeasurementStartTimeLocal
-
-        NumRows
-        NumCols
+       
         PlateMap
 
-        WellInfo
+        ImageInfo
 
     end
-
 
     methods
 
@@ -55,6 +52,31 @@ classdef OPreader
 
             end
         end
+    
+        function I = readImage(obj, row, col, channel, timepoint)
+
+            imageIdx = [obj.ImageInfo.Row] == row & ...
+                [obj.ImageInfo.Col] == col & ...
+                [obj.ImageInfo.ChannelID] == channel & ...
+                [obj.ImageInfo.TimepointID] == timepoint & ...
+                [obj.ImageInfo.FieldID] == 1 & ...
+                [obj.ImageInfo.PlaneID] == 1;
+
+            if nnz(imageIdx) == 0
+                error('OPreader:readImage:InvalidImageCoordinate', ...
+                    'Image at the specified coordinate does not exist.')
+
+            elseif nnz(imageIdx) > 1
+
+                error('OPreader:readImage:ImageCoordinateNotUnique', ...
+                    'Multiple images were found at the specified coordinate.')
+
+            end
+
+            I = obj.ImageInfo(imageIdx).URL;
+            
+        end
+
     end
 
     methods (Access = protected)
@@ -73,43 +95,53 @@ classdef OPreader
             tz = dt.TimeZone;
             obj.MeasurementStartTimeLocal = datetime(obj.MeasurementStartTime,'InputFormat','yyyy-MM-dd''T''HH:mm:ss.SSSSSSSZ','TimeZone',tz);
 
-            obj.NumRows = S.Plates.Plate.PlateRows;
-            obj.NumCols = S.Plates.Plate.PlateCols;
-
             %Gather image information
-            imgRows = cat(1, S.Images.Image.Col);
-            imgCols = cat(1, S.Images.Image.Col);
+            imgLoc = [cat(1, S.Images.Image.Row) cat(1, S.Images.Image.Col)];
 
-            obj.PlateMap = false(obj.NumRows, obj.NumCols);
-            obj.WellInfo = struct('Fields', [], 'Planes', [], ...
-                'Timepoints', [], 'Channels', [], 'ChannelNames', [], ...
-                'Images', []);
+            obj.PlateMap = false(S.Plates.Plate.PlateRows, S.Plates.Plate.PlateColumns);
             
-            %See which wells are populated
-            for iWell = 1:numel(S.Plates.Plate.Well)
+            %Pull image information
+            obj.ImageInfo = S.Images.Image;
 
-                %The well ID is converted into a number. First digit (or
-                %two digits for 4 digit number) is Row, last two digits are
-                %column.
+            %Populate some overview data
+            wellLocs = unique(imgLoc, 'rows');
 
-                wellIDstr = sprintf('%04d', S.Plates.Plate.Well(iWell).idAttribute);
+            obj.PlateMap(wellLocs(:, 1), wellLocs(:, 2)) = true;
 
-                row = str2double(wellIDstr(1:2));
-                col = str2double(wellIDstr(3:4));
-
-                obj.PlateMap(row, col) = true;
-
-                %Pull image information
-                tmpS = S.Images.Image(imgRows == row && imgCols == col);
-
-                obj.WellInfo(row, col).Fields = unique(cat(1, tmpS.FieldID));
-                obj.WellInfo(row, col).Planes = unique(cat(1, tmpS.PlaneID));
-                obj.WellInfo(row, col).Timepoints = unique(cat(1, tmpS.TimepointID));
-                
-
-
-
-            end    
+            % 
+            % %See which wells are populated
+            % for iWell = 1:numel(S.Plates.Plate.Well)
+            % 
+            %     %The well ID is converted into a number. First digit (or
+            %     %two digits for 4 digit number) is Row, last two digits are
+            %     %column.
+            % 
+            %     wellIDstr = sprintf('%04d', S.Plates.Plate.Well(iWell).idAttribute);
+            % 
+            %     row = str2double(wellIDstr(1:2));
+            %     col = str2double(wellIDstr(3:4));
+            % 
+            %     obj.PlateMap(row, col) = true;
+            % 
+            %     %Pull image information
+            %     tmpS = S.Images.Image(imgRows == row & imgCols == col);
+            % 
+            %     obj.WellInfo(iWell).ImageInfo.URL = tmpS.URL;
+            %     obj.WellInfo(iWell).ImageInfo.FieldID = tmpS.FieldID;
+            %     obj.WellInfo(iWell).ImageInfo.PlaneID = tmpS.PlaneID;
+            %     obj.WellInfo(iWell).ImageInfo.TimepointID = tmpS.TimepointID;
+            %     obj.WellInfo(iWell).ImageInfo.ChannelID = tmpS.ChannelID;
+            %     obj.WellInfo(iWell).ImageInfo.ChannelNames = tmpS.ChannelName;
+            % 
+            %     obj.WellInfo(iWell).Row = row;
+            %     obj.WellInfo(iWell).Col = col;
+            %     obj.WellInfo(iWell).Fields = unique(cat(1, tmpS.FieldID));
+            %     obj.WellInfo(iWell).Planes = unique(cat(1, tmpS.PlaneID));
+            %     obj.WellInfo(iWell).Timepoints = unique(cat(1, tmpS.TimepointID));
+            %     obj.WellInfo(iWell).Channels = unique(cat(1, tmpS.ChannelID));
+            %     obj.WellInfo(iWell).ChannelNames = unique(cat(1, tmpS.ChannelName));
+            % 
+            % end    
 
         end
 
